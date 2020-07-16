1b7325232a4b57d56bf13177c27aab891623f1ca
Marlhex <1992ignacio@gmail.com>
2020-07-15 23:01:10 -0700

    Initial Commit

---
+++ ItemRegisterWithCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItemRegisterWithCoreData
+//
+//  Created by Ignacio Arias on 2020-07-15.
+//  Copyright © 2020 Ignacio Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
